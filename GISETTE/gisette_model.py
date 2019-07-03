c740406620e1296831dfc8c6629e6470c0871686
2chenj <habinshutithierry@gmail.com>
2019-07-03 14:36:51 +0200

    percentileSelection for all data

--- GISETTE/gisette_model.py
+++ GISETTE/gisette_model.py
@@ -1,59 +1,66 @@
-''' executing this fails print the accuracy of decision tre on madelon_valid and
-export the prediction on madelon_test.data in madelon_test.predict'''
 
-# diractories variable setting
+""" 	prediction on GISETTE data
+	print the accuracy score of the model on GISETTE trainning dataset
+	export the prediction of the model on GISITTE test dataset
+"""
 
-data_dir = 'MADELON'             
-data_name = 'madelon'
+data_dir = 'GISETTE'             
+data_name = 'gisette'
 #!wc $data_dir/* 
+
 
 # pandas module importation
 import pandas as pd
-#madelon_valid_train.data importation as a dataframe
+#gisette_train.data importation as a dataframe
 #removal of NaN values
 trainData = pd.read_csv(data_dir  + '/' + data_name+"_train.data", sep=" ", header=None)  # The data are loaded as a Pandas Data Frame
 trainData = trainData.dropna(axis=1, how="any")
 #trainData # the standard output dataframe
 
 
-#madelon_valid_train.labels importation as a dataframe
+#gisette_train.labels importation as a dataframe
 #removal of NaN values
 trainLabels = pd.read_csv(data_dir  + '/' + data_name+"_train.labels", sep=" ",header=None, encoding='utf8') 
 trainLabels = trainLabels.dropna(axis=1, how="any")
 #trainLabels # the standard output dataframe
 
-#madelon_valid_valid.data importation as a dataframe
+
+#gisette_valid.data importation as a dataframe
 #removal of NaN values
 validData = pd.read_csv(data_dir  + '/' + data_name+"_valid.data", sep=" ",header=None, encoding='utf8')
 validData=validData.dropna(axis=1,how="any")
 #validData  # the standard output dataframe
 
-#madelon_valid_valid.data importation as a dataframe
+
+#gisettee_valid.labels importation as a dataframe
 #removal of NaN values
 validLabels = pd.read_csv(data_name+"_valid.labels", sep=" ", header=None)  # The data are loaded as a Pandas Data Frame
 validLabels=validLabels.dropna(axis=1,how="any")
 #validLabels  # the standard output dataframe
 
-#madelon_valid.data importation as a dataframe
+
+#gisette_valid.data importation as a dataframe
 #removal of NaN values
 testData = pd.read_csv(data_dir  + '/' + data_name+"_test.data", sep=" ",header=None, encoding='utf8')
 testData=testData.dropna(axis=1,how="any")
-#testData  # the standard output dataframe
 
-from sklearn import tree
-#from sklearn.metrics import accuracy_score
 
-clf = tree.DecisionTreeClassifier()
-clf.fit(trainData, trainLabels)
-labelsPred = clf.predict(testData);
-#acc = accuracy_score(validLabels,labelsPred)
-#acc
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+from sklearn.feature_selection import SelectPercentile
 
-#export the data 
-pd.DataFrame(labelsPred).to_csv("../madelon_test.predict", index=False)
 
-#10-fols cross-validation with DecisionTree
-#from sklearn import tree
+select = SelectPercentile(percentile = 50)
+select.fit(trainData,trainLabels)
+
+selectedTrainData = select.transform(trainData)
+selectedTestData =select.transform(testData) 
+
+#10-fols cross-validation with KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
-clf = tree.DecisionTreeClassifier()
-print (cross_val_score(clf,validData,validLabels,cv=10, scoring="accuracy").mean())
+clf = KNeighborsClassifier(n_neighbors=1,algorithm='brute')
+clf.fit(selectedTrainData, trainLabels)
+labelsPred = clf.predict(selectedTestData);
+pd.DataFrame(labelsPred).to_csv("../gisette_test.predict", index=False,header=False)
+print (cross_val_score(clf,selectedTrainData,trainLabels,cv=10, scoring="accuracy").mean())
+
--- MADELON/madelon_model.py
+++ MADELON/madelon_model.py
@@ -1,27 +1,22 @@
+""" prediction on MADELON data
 
-""" 	prediction on MADELON data
-	print the occuracy score  of the model on ARCENE trainng dataset 
-	export the model prediction on ARCENE test dataset
- """
-
-
+	prints the score of the model on MADELON training Set with closs validation
+	export the model prediction on MADELON test set
+"""
 
 
-#IMPORTATION OF DATASETS
 
-# diretories variable setting
-data_dir = 'ARCENE'             
-data_name = 'arcene'
-#!wc $data_dir/* 
+##IMPORTATION DES DONNEES
 
 
+# diractories variable setting
+data_dir = 'MADELON'             
+data_name = 'madelon'
 # pandas module importation
+
 import pandas as pd
-
-
-#arcene_train.data importation as a dataframe
-
-trainData = pd.read_csv(data_dir  + '/' + data_name+"_train.data", sep=" ", header=None)  # The data 
+#madelon_valid_train.data importation as a dataframe
+trainData = pd.read_csv(data_dir  + '/' + data_name+"_train.data", sep=" ", header=None)  # The data are loaded as a Pandas Data Frame
 trainData = trainData.dropna(axis=1, how="any") #removal of NaN values
 
 trainLabels = pd.read_csv(data_dir  + '/' + data_name+"_train.labels", sep=" ",header=None, encoding='utf8') 
@@ -33,27 +28,28 @@ validData=validData.dropna(axis=1,how="any")
 validLabels = pd.read_csv(data_name+"_valid.labels", sep=" ", header=None)  # The data are loaded as a Pandas Data Frame
 validLabels=validLabels.dropna(axis=1,how="any")
 
-testData = pd.read_csv(data_dir  + '/' + data_name+"_test.data", sep=" ",header=None, encoding='utf8')  # The data are loaded as a Pandas Data Frame
+testData = pd.read_csv(data_dir  + '/' + data_name+"_test.data", sep=" ",header=None, encoding='utf8')
 testData=testData.dropna(axis=1,how="any")
 
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn import tree
 import numpy as np
 from sklearn.feature_selection import SelectPercentile
 
-#FEATURES SELECTION
-select = SelectPercentile(percentile = 50)
+# FEATURES SELECTION
+select = SelectPercentile(percentile = 65)
 select.fit(trainData,trainLabels)
 
 selectedTrainData = select.transform(trainData)
 selectedTestData =select.transform(testData) 
 
-#PREDICTION AND EXPORTS
-#10-fols cross-validation with KNeighborsClassifier
+#PREDICTION AND EXPORT
+
+#10-fols cross-validation with DecisionTree
 from sklearn.model_selection import cross_val_score
-clf = KNeighborsClassifier(n_neighbors=1,algorithm='brute')
+clf =tree.DecisionTreeClassifier()
 clf.fit(selectedTrainData, trainLabels)
 labelsPred = clf.predict(selectedTestData);
-pd.DataFrame(labelsPred).to_csv("../arcene_test.predict", index=False,header=False)
+pd.DataFrame(labelsPred).to_csv("../madelon_test.predict", index=False , header=False) #exportation of labelsPred
 print (cross_val_score(clf,selectedTrainData,trainLabels,cv=10, scoring="accuracy").mean())
 
 
